3c6135f5f111fac24cde5a58b78a4204f556e28e
Abdelrahman Walid Ahmed <abdualrahmanwaleed45@gmail.com>
2025-11-24 12:13:26 +0200

    Fixes

--- app/src/main/java/com/byte2bites/app/OrdersFragment.kt
+++ app/src/main/java/com/byte2bites/app/OrdersFragment.kt
@@ -12,7 +12,6 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import android.widget.Toast
-import androidx.core.app.ActivityCompat
 import androidx.core.app.NotificationCompat
 import androidx.core.app.NotificationManagerCompat
 import androidx.core.content.ContextCompat
